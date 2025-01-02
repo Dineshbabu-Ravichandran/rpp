360c8dfd28ce65f3dcb6ee56760b9ab3d7cbac94
dineshbabu-ravichandran <dineshbabu.ravichandran@multicorewareinc.com>
2025-01-02 13:32:51 +0000

    Minor changes

--- utilities/test_suite/rpp_test_suite_misc.h
+++ utilities/test_suite/rpp_test_suite_misc.h
@@ -374,7 +374,6 @@ void compare_output(Rpp32f *outputF32, Rpp32u nDim, Rpp32u batchSize, Rpp32u buf
         for(int j = 0; j < sampleLength; j++)
         {
             bool invalid_comparision = ((out[j] == 0.0f) && (ref[j] != 0.0f));
-            invalid_comparision = false;
             if(!invalid_comparision && abs(out[j] - ref[j]) < 1e-4)
                 cnt++;
         }
